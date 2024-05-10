02ef15d420aaeeead50ea04dfa7ec6a2f4412a01
walidNas <nas00waleed@gmail.com>
2024-05-10 19:24:30 +0400

    added stripe

---
+++ SmartWash/SmartWash.Application/PaymentSystem/PaymentService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartWash.Domain.Entities;
+using SmartWash.Domain.Interfaces;
+using Stripe;
+
+namespace SmartWash.Application.PaymentSystem
+{
+	public class PaymentService
+	{
+		private readonly IStripeAdapter _stripeAdapter;
+
+		public PaymentService(IStripeAdapter paymentService)
+		{
+			_stripeAdapter = paymentService;
+		}
+
+		public async Task<Charge> MakePaymentAsync(CreditCard creditCard, decimal amount, string currency)
+		{
+			var token = await _stripeAdapter.CreateCardTokenAsync(creditCard.CardNumber, creditCard.ExpirationMonth,
+				creditCard.ExpirationYear, creditCard.CVV);
+
+			if (token == null)
+			{
+				throw new Exception("Failed to create card token");
+			}
+
+			var createdPayment = await _stripeAdapter.CreateChargeAsync(amount, currency, token);
+			return createdPayment;
+		}
+	}
+}
---
+++ SmartWash/SmartWash.Infrastructure/Stripe/StripeAdapter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartWash.Domain.Interfaces;
+using Stripe;
+
+namespace SmartWash.Infrastructure.Stripe
+{
+	public class StripeAdapter : IStripeAdapter
+	{
+		private readonly StripeClient _stripeClient;
+
+		public StripeAdapter(string apiKey)
+		{
+			_stripeClient = new StripeClient(apiKey);
+		}
+
+		public async Task<Charge> CreateChargeAsync(decimal amount, string currency, string cardToken)
+		{
+			var options = new ChargeCreateOptions
+			{
+				Amount = (long)(amount * 100), // Stripe requires amount in cents
+				Currency = currency,
+				Source = cardToken
+			};
+
+			var service = new ChargeService(_stripeClient);
+			return await service.CreateAsync(options);
+		}
+
+		public async Task<string> CreateCardTokenAsync(string cardNumber, int expMonth, int expYear, string cvc)
+		{
+			var tokenOptions = new TokenCreateOptions
+			{
+				Card = new TokenCardOptions
+				{
+					Number = cardNumber,
+					ExpMonth = expMonth.ToString(),
+					ExpYear = expYear.ToString(),
+					Cvc = cvc
+				}
+			};
+
+			var tokenService = new TokenService(_stripeClient);
+			var token = await tokenService.CreateAsync(tokenOptions);
+
+			return token.Id;
+		}
+
+	}
+}
